2a847d8826c7f3f2be5b9b40bb1ddfd27f9fada9
1notchi <inoyuuki1@gmail.com>
2026-07-20 19:48:38 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const GAS_WEB_APP_URL = "https://script.google.com/macros/s/AKfycby0uPuFr7-YtjMtL-Xc5FNvs4QkCpbfsiT14_ubEteC1ElhFEKmWm-v9mt5KKN6dOkiFA/exec";
+//const GAS_WEB_APP_URL = "";
 
 const CONFIG = {
     SUPABASE_URL: "https://huexygwgyyftvzecyjjp.supabase.co",
