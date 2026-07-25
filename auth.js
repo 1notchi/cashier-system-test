37369f677d17e0b2c62450348b81437d8d13934e
1notchi <inoyuuki1@gmail.com>
2026-07-25 19:44:09 +0900

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,3 +1,9 @@
+// Supabaseクライアント生成
+const mySupabase = window.supabase.createClient(
+    CONFIG.SUPABASE_URL,
+    CONFIG.SUPABASE_KEY
+);
+
 //ログインしていなかったらログイン画面に戻す
 window.addEventListener("pageshow", async () => {
     const { data: { session } } = await mySupabase.auth.getSession();
