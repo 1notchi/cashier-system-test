84f13649bc78541e41716994d657ec40e041c3ef
1notchi <inoyuuki1@gmail.com>
2026-07-26 22:10:19 +0900

    Delete headerLoader.js

--- headerLoader.js
+++
@@ -1,8 +0,0 @@
-async function loadHeader() {
-    const response = await fetch("components/header.html");
-    const html = await response.text();
-    document.getElementById("header").innerHTML = html;
-    initializeHeader();
-}
-
-document.addEventListener("DOMContentLoaded", loadHeader);
